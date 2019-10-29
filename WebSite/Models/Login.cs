1379106a4ecaa21769e2db3ba637f8c7bbb7a4c0
GabrimasterUMG <56774736+GabrimasterUMG@users.noreply.github.com>
2019-10-28 22:06:21 -0600

    Segunda modificacion de Login
    Modificacion de mensajes de error

--- WebSite/Models/Login.cs
+++ WebSite/Models/Login.cs
@@ -9,9 +9,9 @@ namespace WebSite.Models
     public partial class Login
     {
         [Key]
-        [Required(ErrorMessage="Ingrese el usuario:",AllowEmptyStrings=false)]
+        [Required(ErrorMessage="Ingrese el usuario correcto:",AllowEmptyStrings=false)]
         public string ID_USUARIO { get; set; }
-        [Required(ErrorMessage = "Ingrese el contraseña:", AllowEmptyStrings = false)]
+        [Required(ErrorMessage = "Ingrese el contraseña correcta:", AllowEmptyStrings = false)]
         [DataType(System.ComponentModel.DataAnnotations.DataType.Password)]
         public string CONTRASEÑA { get; set; }
         public string TIPO { get; set; }
